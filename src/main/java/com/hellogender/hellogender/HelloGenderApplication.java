a2045fdba3a365b8f4508d231437bf6da46e53f6
Kamil <kamilpieniazzek96@gmail.com>
2021-03-31 19:37:23 +0200

    Initial commit

---
+++ src/main/java/com/hellogender/hellogender/HelloGenderApplication.java
@@ -0,0 +1,13 @@
+package com.hellogender.hellogender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGenderApplication.class, args);
+	}
+
+}
