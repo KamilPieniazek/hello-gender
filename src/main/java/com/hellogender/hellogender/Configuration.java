cf38a5a9032ddcd6cf788eb432d550544cfe73e1
Kamil <kamilpieniazzek96@gmail.com>
2021-04-05 22:21:26 +0200

    Fix .jar packaging

--- src/main/java/com/hellogender/hellogender/Configuration.java
+++ src/main/java/com/hellogender/hellogender/Configuration.java
@@ -1,6 +1,6 @@
 package com.hellogender.hellogender;
 
 public class Configuration {
-    public static String femaleTokenPath="tokens/female_firstnames.txt";
-    public static String maleTokenPath="tokens/male_firstnames.txt";
+    public static String femaleTokenPath= "/tokens/female_firstnames.txt";
+    public static String maleTokenPath= "/tokens/male_firstnames.txt";
 }
